8122cd0d753b8cdda0ee0c0cfe6a6772dae22f21
Naga Anjaneyulu <nakopa@iu.edu>
2020-12-02 16:52:54 -0500

    Deploy

--- src/UIService.js
+++ src/UIService.js
@@ -4,14 +4,25 @@ import axios from 'axios';
 class UIService{
 
 
-    startAssesment(data){
+    submitUrl(data){
 
-       return axios.post('http://127.0.0.1:5000/start', 
-       data)
+       return axios.post('https://us-central1-naga-kopalle2.cloudfunctions.net/naga-faas1',{  headers : {
+        'Content-Type': 'application/json'
+    },
+       data})
 
     }
 
-   
+    getFreImage(data){
+        return axios.get('https://us-central1-naga-kopalle2.cloudfunctions.net/visualize_data',
+        data,{  headers : {
+            'Content-Type': 'application/json'
+        }})
+ 
+     }
+ 
+
+
 }
 
 export default new UIService()
